b55495dd66bfbbfceca81ebf849fc229e335c3c8
evazion <noizave@gmail.com>
2024-06-09 15:13:49 -0500

    Use Concurrent.available_processor_count instead of Etc.nprocessors.
    Use `Concurrent.available_processor_count` so that we take into account CPU limits inside
    containers, instead of using all the cores across the entire machine.
    
    For example, if you have 8 physical CPU cores, but you're running Danbooru inside a container
    limited to 2 CPU cores, now Danbooru will use 2 cores instead of 8 cores.

--- config/danbooru_default_config.rb
+++ config/danbooru_default_config.rb
@@ -197,7 +197,7 @@ module Danbooru
     #
     # Set this to 0 to disable multithreading. This may save memory at the cost of reduced performance.
     def max_concurrency
-      Etc.nprocessors
+      Concurrent.available_processor_count.to_i.clamp(1..)
     end
 
     # If true, allow web crawlers such as Google to crawl your site.
